e3ad8f788ab3bc618600755c3ceb3d43106bbe5e
JunCastle <juncastle97@gmail.com>
2025-01-20 14:10:50 +0900

    fix : button hover 수정

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -17,9 +17,9 @@ export default function Button({ label, onClick, variant = 'primary' }: ButtonPr
     secondary: 'bg-mint-500 hover:bg-mint-400',
     disabled: 'bg-gray-300 cursor-not-allowed',
     outlinePrimary:
-      'bg-white border border-orange-400 !text-orange-400 hover:border-orange-300 hover:text-orange-300',
+      'bg-white border border-orange-400 !text-orange-400 hover:border-orange-300 hover:!text-orange-300',
     outlineSecondary:
-      'bg-white border border-mint-500 !text-mint-500 hover:border-mint-400 hover:text-mint-400',
+      'bg-white border border-mint-500 !text-mint-500 hover:border-mint-400 hover:!text-mint-400',
     outlineDisabled: 'bg-white border border-gray-300 !text-gray-300 cursor-not-allowed',
   };
 
