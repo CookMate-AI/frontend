79b08a005d63254b6c91687cbe02700357181c78
JunCastle <juncastle97@gmail.com>
2025-01-27 14:05:24 +0900

    feat : dropdown 구현

--- src/components/common/Gnb/index.tsx
+++ src/components/common/Gnb/index.tsx
@@ -1,25 +1,32 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import useDropdownStore from '@/stores/useDropdownStore';
+import Dropdown from './Dropdown';
 
 export default function Gnb() {
   const router = useRouter();
+  const { openDropdown } = useDropdownStore();
 
   return (
-    <div className="h-100 flex w-full items-center justify-between bg-orange-400 px-24 py-16">
+    <div className="flex h-100 w-full items-center justify-between bg-orange-400 px-24 py-16">
       <div
-        className="text-20 flex cursor-pointer items-center justify-center text-navy"
+        className="flex cursor-pointer items-center justify-center text-20 text-navy"
         onClick={() => router.push('/')}
       >
         <Image src={'/icons/ic-logo.svg'} alt="logo" width={80} height={80} />
         <div>Cook Mate</div>
       </div>
-      <Image
-        src={'/icons/ic-profile.svg'}
-        alt="profile"
-        width={80}
-        height={80}
-        className="cursor-pointer"
-      />
+      <div className="relative">
+        <Image
+          src={'/icons/ic-profile.svg'}
+          alt="profile"
+          width={80}
+          height={80}
+          className="cursor-pointer"
+          onClick={openDropdown}
+        />
+        <Dropdown />
+      </div>
     </div>
   );
 }
