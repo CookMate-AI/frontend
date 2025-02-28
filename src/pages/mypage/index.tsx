2aa3349cc321f21dd55f79c3879508d05466465f
JunCastle <juncastle97@gmail.com>
2025-02-28 15:15:56 +0900

    feat : 유저 닉네임을 마이페이지에서 보여주기

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -2,16 +2,18 @@ import EditProfile from '@/components/Mypage/EditProfile';
 import MyRecipes from '@/components/Mypage/MyRecipes';
 import Image from 'next/image';
 import { useState } from 'react';
+import useNicknameStore from '@/stores/useNicknameStore';
 
 export default function Mypage() {
   const [activeTab, setActiveTab] = useState('recipes');
+  const { nickname } = useNicknameStore();
 
   return (
     <div className="flex gap-70 px-80 py-100">
       <div className="h-430 w-230 overflow-hidden rounded-10 border-2 border-gray-400 bg-white">
         <div className="flex h-270 w-full flex-col items-center justify-center gap-24 border-b-2 border-gray-400">
           <Image src={'/icons/ic-profile-black.svg'} alt="profile" width={85} height={85} />
-          <p className="text-20 font-bold">닉네임</p>
+          <p className="text-20 font-bold">{nickname}</p>
         </div>
         <div
           className={`flex h-80 w-full cursor-pointer items-center justify-center border-b-2 border-gray-400 text-24 font-bold hover:bg-orange-200 ${
