7e9f6d7a6af537c564840ca391548e44251fd452
JunCastle <juncastle97@gmail.com>
2025-03-04 13:35:41 +0900

    fix : mypage 반응형 수정 진행 중

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -9,14 +9,16 @@ export default function Mypage() {
   const { nickname } = useNicknameStore();
 
   return (
-    <div className="flex gap-70 px-80 py-100">
-      <div className="h-430 w-230 overflow-hidden rounded-10 border-2 border-gray-400 bg-white">
-        <div className="flex h-270 w-full flex-col items-center justify-center gap-24 border-b-2 border-gray-400">
-          <Image src={'/icons/ic-profile-black.svg'} alt="profile" width={85} height={85} />
-          <p className="text-20 font-bold">{nickname}</p>
+    <div className="flex gap-30 px-40 py-60 lg:gap-70 lg:px-80 lg:py-100">
+      <div className="h-260 min-w-100 overflow-hidden rounded-10 border-2 border-gray-400 bg-white lg:h-430 w-230">
+        <div className="flex h-160 w-full flex-col items-center justify-center gap-24 border-b-2 border-gray-400 lg:h-270">
+          <div className="relative h-50 w-50 lg:h-85 lg:w-85">
+            <Image src={'/icons/ic-profile-black.svg'} alt="profile" fill />
+          </div>
+          <p className="text-16 font-bold lg:text-20">{nickname}</p>
         </div>
         <div
-          className={`flex h-80 w-full cursor-pointer items-center justify-center border-b-2 border-gray-400 text-24 font-bold hover:bg-orange-200 ${
+          className={`flex h-50 w-full cursor-pointer items-center justify-center border-b-2 border-gray-400 text-14 font-bold hover:bg-orange-200 lg:h-80 lg:text-24 ${
             activeTab === 'recipes' ? 'bg-orange-200' : ''
           }`}
           onClick={() => setActiveTab('recipes')}
@@ -24,7 +26,7 @@ export default function Mypage() {
           나의 레시피
         </div>
         <div
-          className={`flex h-80 w-full cursor-pointer items-center justify-center rounded-b-10 text-24 font-bold hover:bg-orange-200 ${
+          className={`flex h-50 w-full cursor-pointer items-center justify-center rounded-b-10 pb-2 text-14 font-bold hover:bg-orange-200 lg:h-80 lg:text-24 ${
             activeTab === 'profile' ? 'bg-orange-200' : ''
           }`}
           onClick={() => setActiveTab('profile')}
@@ -32,7 +34,7 @@ export default function Mypage() {
           개인정보 수정
         </div>
       </div>
-      <div className="h-850 w-1000 overflow-y-auto rounded-10 border-2 border-gray-400 bg-white scrollbar-hide">
+      <div className="h-750 min-w-550 w-1000 overflow-y-auto rounded-10 border-2 border-gray-400 bg-white scrollbar-hide lg:h-850">
         {activeTab === 'recipes' ? <MyRecipes /> : <EditProfile />}
       </div>
     </div>
